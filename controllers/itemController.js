31cd99006680f5b487a9e3de23d81a81389a00cd
Brandon Araujo <bac2795@gmail.com>
2025-06-16 01:21:57 -0500

    Adding github actions

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,9 +1,8 @@
-// itemController.js
-
 const { faker } = require('@faker-js/faker');
-const { getSecrets } = require('../libs/secrets');
 
 let items = [];
+
+API_PROVIDER_URL = process.env.API_PROVIDER_URL || 'localhost';
 
 // Generar datos fake si está vacío
 function generateFakeItems(count = 10) {
@@ -16,13 +15,10 @@ function generateFakeItems(count = 10) {
 }
 
 // Endpoint: GET /api/items
-exports.getAllItems = async (req, res) => {
+exports.getAllItems = (req, res) => {
   if (items.length === 0) {
     items = generateFakeItems(10);
   }
-  const secrets = await getSecrets();
-  const API_PROVIDER_URL = secrets.API_PROVIDER_URL || 'localhost';
-
   res.status(200);
   res.setHeader('Content-Type', 'application/json');
   res.json({
@@ -34,11 +30,9 @@ exports.getAllItems = async (req, res) => {
 };
 
 // Endpoint: GET /api/items/:id
-exports.getItem = async (req, res) => {
+exports.getItem = (req, res) => {
   const item = items.find(i => i.id === req.params.id);
-  const secrets = await getSecrets();
-  const API_PROVIDER_URL = secrets.API_PROVIDER_URL || 'localhost';
-
+  
   res.setHeader('Content-Type', 'application/json');
   item ? res.json({
     metadata: {
@@ -49,19 +43,16 @@ exports.getItem = async (req, res) => {
 };
 
 // Endpoint: POST /api/items
-exports.createItem = async (req, res) => {
-  const newItem = { id: faker.string.uuid(), ...req.body };
-  if (!newItem.name || !newItem.email || !newItem.registeredAt) {
-    res.status(400).send('Missing required fields: name, email, or registeredAt');
-    return;
-  }
-  items.push(newItem);
-
-  const secrets = await getSecrets();
-  const API_PROVIDER_URL = secrets.API_PROVIDER_URL || 'localhost';
-
-  res.setHeader('Content-Type', 'application/json');
-  res.status(201).json({
+exports.createItem = (req, res) => {
+    const newItem = { id: faker.string.uuid(), ...req.body };
+    if (!newItem.name || !newItem.email || !newItem.registeredAt) {
+        res.status(400).send('Missing required fields: name, email, or registeredAt');
+        return;
+    }
+    items.push(newItem);
+    
+    res.setHeader('Content-Type', 'application/json');
+    res.status(201).json({
     metadata: {
       provider: API_PROVIDER_URL
     },
@@ -70,32 +61,25 @@ exports.createItem = async (req, res) => {
 };
 
 // Endpoint: PUT /api/items/:id
-exports.updateItem = async (req, res) => {
+exports.updateItem = (req, res) => {
   const index = items.findIndex(i => i.id === req.params.id);
   if (index !== -1) {
     items[index] = { ...items[index], ...req.body };
+    
+  res.setHeader('Content-Type', 'application/json');
+    res.json(items[index]);
 
-    const secrets = await getSecrets();
-    const API_PROVIDER_URL = secrets.API_PROVIDER_URL || 'localhost';
-
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-      metadata: {
-        provider: API_PROVIDER_URL
-      },
-      item: items[index]
-    });
-  } else {
-    res.status(404).send('Item not found');
-  }
+  res.setHeader('Content-Type', 'application/json');  
+} else res.status(404).send('Item not found');
 };
 
 // Endpoint: DELETE /api/items/:id
-exports.deleteItem = async (req, res) => {
+exports.deleteItem = (req, res) => {
   const initialLength = items.length;
   items = items.filter(i => i.id !== req.params.id);
   const deleted = items.length < initialLength;
 
+  
   res.setHeader('Content-Type', 'application/json');
   res.status(deleted ? 204 : 404).send();
 };
